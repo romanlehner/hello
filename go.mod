dcb4945cf6091c95ded592b88349f23a2b5b9b36
Roman Lehner <romanlehner@airasia.com>
2020-09-07 15:11:08 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.15
+
+require github.com/romanlehner/greetings v0.0.0-20200907055410-613873834e2f
---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/romanlehner/greetings"
+)
+
+func main() {
+	fmt.Println(greetings.HelloWorld())
+
+}
